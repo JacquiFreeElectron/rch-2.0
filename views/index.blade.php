6179434fa0788632d8f7c60fb4ae3bf61e82822a
JacquiFreeElectron <jacqui@free-electron.org>
2023-03-25 04:39:44 +0000

    delete jianti

--- views/index.blade.php
+++ views/index.blade.php
@@ -10,7 +10,7 @@
 
     <div class="intro">
     <div><h6><img src="images/icon/indexpagelogo/earth.svg" alt="earth" width="50" height="50"> 什么是润潮会？</h6>
-    <div class="p"><p>润潮会是以简体中文为母语者（准备出国，正在出国，开始考虑出国）的网络互助会</p></div>
+    <div class="p"><p>润潮会是以中文为母语者（准备出国，正在出国，开始考虑出国）的网络互助会</p></div>
     </div>
     <div><h6><img src="images/icon/indexpagelogo/wh.svg" alt="?" width="50" height="50"></img>润潮会对会员意味着什么？</h6>
     <div class="p"><p>润潮会是会员的圈子、家园、可以栖息的大地</p></div></div>
